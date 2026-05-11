5e656f2bfb823a0d8607f6be55a49958294dc8be
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 11:27:37 +0700

    Menambahkan definisi kelas user dengan atribut, konstruktor, dan metode untuk menghasilkan ID global

--- user.h
+++ user.h
@@ -3,3 +3,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class user {
+public:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+    user(string pNama, string pEmail) : nama(pNama), email(pEmail) {
+        id = generateId();
+    }
+    int generateId() {
+        return ++globalId;
+    }
+};
+int user::globalId = 0;
+#endif
