0b982213fd1b91008d9ead9c099bf6ca38987216
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 10:53:31 +0700

    Memperbaiki urutan pemanggilan fungsi dan menambahkan penghapusan objek untuk varIbu dan varIbu2

--- filemain.cpp
+++ filemain.cpp
@@ -18,3 +18,9 @@ int main() { //agregasi
 
     varIbu->cetakAnak();
     varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
