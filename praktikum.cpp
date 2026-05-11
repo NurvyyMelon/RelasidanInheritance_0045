9fe58d7002011b486b17d90e0d992e67141e0179
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 11:33:21 +0700

    Memperbarui fungsi main untuk menampilkan profil member dan mengubah status keaktifan member oleh admin

--- praktikum.cpp
+++ praktikum.cpp
@@ -12,3 +12,28 @@ using namespace std;
 int main() {
     // Membuat objek admin pakai pointer (biar mirip kodinganmu yang lain)
     admin* varAdmin = new admin("Makoto Yuki", "admin@umy.ac.id");
+
+    // Membuat daftar member menggunakan vector
+    vector<member> daftarMember;
+    daftarMember.push_back(member("Fuuka", "Fuuka@gmail.com"));
+    daftarMember.push_back(member("Yukari", "Yukari@gmail.com"));
+
+    // 1. Admin menampilkan semua member
+    varAdmin->showAllMember(daftarMember);
+
+    // 2. Member pertama pamer profil
+    cout << "--- Profil Sebelum Diubah Admin ---" << endl;
+    daftarMember[0].showProfile();
+
+    // 3. Admin mengubah status member pertama (Toggle Activation)
+    varAdmin->toggleActivationMember(daftarMember[0]);
+
+    // 4. Member pertama cek profil lagi (statusnya pasti berubah)
+    cout << "--- Profil Setelah Diubah Admin ---" << endl;
+    daftarMember[0].showProfile();
+
+    // Hapus pointer biar nggak leak memori
+    delete varAdmin;
+
+    return 0;
+}
