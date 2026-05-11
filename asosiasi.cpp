2bc66abc10831040c07e41aa4da3545ca88011bc
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 11:01:27 +0700

    Menambahkan definisi atribut dan konstruktor pada kelas pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -4,3 +4,9 @@ using namespace std;
 
 class dokter;
 class pasien { // asosiasi
+  public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
