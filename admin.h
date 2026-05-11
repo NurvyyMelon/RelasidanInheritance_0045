02fe9ef8bf5a4e91bcd188080457f8133d7a04e2
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 11:31:15 +0700

    Menambahkan kelas admin yang dari user dengan metode untuk menampilkan semua anggota dan mengubah status keaktifan anggota

--- admin.h
+++ admin.h
@@ -3,3 +3,18 @@
 #include <vector>
 #include "user.h"
 #include "member.h"
+
+class admin : public user {
+public:
+    admin(string pNama, string pEmail) : user(pNama, pEmail) {}
+
+    void showAllMember(vector<member>& daftar) {
+        for (int i = 0; i < daftar.size(); i++) {
+            cout << i+1 << ". " << daftar[i].nama << " [" << daftar[i].status << "]" << endl;
+        }
+    }
+    void toggleActivationMember(member& m) {
+        m.status = (m.status == "Aktif") ? "Non-Aktif" : "Aktif";
+    }
+};
+#endif
