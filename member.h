96e92084ea51767e559c57063d695c47d7bc3860
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-11 11:29:25 +0700

    Menambahkan kelas member yang mewarisi dari user dengan atribut status dan metode showProfile

--- member.h
+++ member.h
@@ -1,3 +1,17 @@
 #ifndef MEMBER_H
 #define MEMBER_H
 #include "user.h"
+
+class member : public user { // Turunan dari user
+public:
+    string status; 
+    // id, nama, email tidak ditulis lagi karena sudah warisan dari user
+
+    member(string pNama, string pEmail) : user(pNama, pEmail) {
+        status = "Aktif";
+    }
+    void showProfile() {
+        cout << "Nama: " << nama << "\nID: " << id << "\nStatus: " << status << endl;
+    }
+};
+#endif
